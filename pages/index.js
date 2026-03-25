e4a48f84d907f115934e11ab5b0b7481f471b58e
Chewie <matthewchuang04@gmail.com>
2026-03-25 03:47:00 -0700

    checkmark removed

--- pages/index.js
+++ pages/index.js
@@ -562,7 +562,7 @@ export default function Home() {
                 </span>
               </a>
               <span className="hero-count">
-                <strong>100+</strong> on the list
+                <strong>121</strong> on the list
               </span>
             </div>
           </div>
